723565458c976b6455c63dcf104cc5218da23fa7
BrianGaynor <32244479+BrianGaynor@users.noreply.github.com>
2017-09-25 01:18:59 +0100

    testing out Markov Chain

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 # for tying he flow of the application together
+
+from markov_python.cc_markov import MarkovChain
+
+for i in range (1,12):
+  if i % 2 == 0:
+    print i ** 2
+	
+mc = MarkovChain()
